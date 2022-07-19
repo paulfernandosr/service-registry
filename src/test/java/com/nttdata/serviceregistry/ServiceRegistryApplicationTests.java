fb08f4281ce746e6e5d6e7757ad8ede0604ed0f2
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-19 18:56:08 -0500

    update for project 3

--- src/test/java/com/nttdata/serviceregistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/nttdata/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,12 +1,10 @@
 package com.nttdata.serviceregistry;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceRegistryApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
